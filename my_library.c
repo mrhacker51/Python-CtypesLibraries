68f42151f95d9fef6ef984dfc34cb17da037aad5
Cyber Security <59092167+mrhacker51@users.noreply.github.com>
2020-11-10 12:10:28 -0500

    Add files via upload

---
+++ my_library.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int topla(int a, int b);
+int cikar(int a,int b);
+int carp(int a,int b);
+
+int topla(int a,int b){
+	return a+b;
+}
+
+int cikar(int a,int b){
+	return a -b;
+}
+
+int carp(int a, int b){
+	return a * b;
+}
+
+
